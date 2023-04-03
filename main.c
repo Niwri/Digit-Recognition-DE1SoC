de18b55dd122fc9944d37755bdbba9cbcdca308a
Irwin <irwinngo@gmail.com>
2023-04-03 14:27:34 -0400

    Added He Initialization method for weights. Beginning Froward Propagation on the TrainModel method.

--- main.c
+++ main.c
@@ -52,24 +52,26 @@ double crossEntropy(double* predictedProbabilities, double* targetProbabilities,
 *************************************************************************************/
 
 // No Activation Function. Returns the same vector.
-double* noActivation(double* vector, int vectorSize) {
+double* noActivationVector(double* vector, int vectorSize) {
     return vector;
 }
 
 
 // ReLu Activation Function. Returns a vector of max(0, x)
-double* reLu(double* vector, int vectorSize) {
+double* reLuVector(double* vector, int vectorSize) {
 
     double* output = (double*)malloc(vectorSize * sizeof(double));
 
     for(int i = 0; i < vectorSize; i++) 
         output[i] = vector[i] > 0 ? vector[i] : 0;
 
+    free(vector);
+
     return output;
 }
 
 // Softmax Activation Function. Returns a vector of e^x / sum(e^x_i)
-double* softMax(double* vector, int vectorSize) {
+double* softMaxVector(double* vector, int vectorSize) {
     double exp_sum = 0;
 
     for(int i = 0; i < vectorSize; i++)
@@ -80,6 +82,8 @@ double* softMax(double* vector, int vectorSize) {
     for(int i = 0; i < vectorSize; i++) 
         output[i] = exp(vector[i]) / exp_sum;
     
+    free(vector);
+
     return output;
 }
 
@@ -125,7 +129,7 @@ double linearNode(double* input, int inputSize, double* weights, int weightSize,
 *************************************************************************************/
 
 double* linearLayer(double* input, int inputSize, double** weightArrays, int weightSize, int numOfWeightArrays, 
-                    double* bias, int biasSize, int numOfNodes, activation_func_vector_ptr activation_func) {
+                    double* bias, int biasSize, int numOfNodes) {
 
     // Check if inputted inputSize matches weightSize
     if(inputSize != weightSize) {
@@ -149,18 +153,300 @@ double* linearLayer(double* input, int inputSize, double** weightArrays, int wei
         Note: At this point, numOfWeightArrays == numOfNodes == biasSize.
     */
 
-    double* outputWithoutActivation = (double*)malloc(numOfNodes * sizeof(double));
+    double* output = (double*)malloc(numOfNodes * sizeof(double));
 
     for(int i = 0; i < numOfNodes; i++)
-        outputWithoutActivation[i] = linearNode(input, inputSize, weightArrays[i], weightSize, bias[i]);
-    
-    double* outputWithActivation = activation_func(outputWithoutActivation, numOfNodes);
+        output[i] = linearNode(input, inputSize, weightArrays[i], weightSize, bias[i]);
 
-    free(outputWithoutActivation);
-
-    return outputWithActivation;
+    return output;
 
 }
+
+
+/************************************************************************************
+*                                                                                   *
+*   WEIGHT INITIALIZATION                                                           *
+*                                                                                   *
+*************************************************************************************/
+
+// He Initialization Method of Weights. Considering no negative input for MNIST, 
+double* HeInitialization(int numOfPreviousNeurons) {
+    double* weights = (double*)malloc(numOfPreviousNeurons * sizeof(double));
+
+    for(int i = 1; i <= numOfPreviousNeurons; i++)
+        weights[i] = sqrt(2 / i);
+    
+    return weights;
+}
+
+/************************************************************************************
+*                                                                                   *
+*   TRAINING FUNCTIONS                                                              *
+*                                                                                   *
+*************************************************************************************/
+
+/*  Forward Propagation 
+    Parameters: 1D Example Input, Example Size,
+                3D Weight Array, Number of Weights Array, Weights Array Size,
+                2D Biases, Number of Bias Arrays,
+    
+    Returns: 1D Output Vector 
+*/
+
+
+/*
+    Train Model Function.
+    Parameters: 2D Input of multiple 1D Examples Array, Number of examples, Example size, 
+                Empty 3D Weights array, Number of weights array, weights array size, 
+                Empty 2D Biases, Number of bias arrays,
+                Batch size, Number of epochs, Learning Rate
+    Returns:    Updated Weights and Bias Arrays
+
+    Input[Example][Index]
+    Weights[Layer][Node][Index]
+    Bias[Layer][Node]
+
+    NOTE: THE MODEL LAYERS ARE PRE-DEFINED (Possible to "Add" layers before training?)
+*/
+void trainModel(double** input, int numOfExamples, int exampleSize, 
+                double*** weights, int numOfWeightArrays, int weightSize,
+                double** bias, int numOfBiasArrays, 
+                int batchSize, int epochs, double learningRate) {
+
+    // Initialize weights based on He Initialization
+
+}
+
+
+
+/************************************************************************************
+*                                                                                   *
+*   TEST FUNCTIONS                                                                  *
+*                                                                                   *
+*************************************************************************************/
+
+void testLinearNode() {
+
+    double input[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    double weights[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    double bias = 5;
+
+    int inputSize = sizeof(input) / sizeof(input[0]);
+    int weightSize = sizeof(weights) / sizeof(weights[0]);
+
+    double output = linearNode(input, inputSize, weights, weightSize, bias);
+    printf("Linear Node Test:\n");
+    printf("\t Input size: %d\n", inputSize);
+    printf("\t Weight size: %d\n", weightSize);
+    printf("\t Output: %lf\n", output);
+    printf("\t Expected: 209.0");
+}
+
+void testLinearLayer() {
+    
+    int inputSize = 4;
+    double input[] = {1, 2, 3, 4}; 
+    
+    int numOfWeightArrays = 3;
+    int weightSize = 4;
+
+    double weightArraysToCopy[3][4] = {{1, 2, 3, -4}, {1, 1, 1, 1,}, {0.5, 0.5, 0.5, 0.5}};
+
+    double **weightArrays = (double**)malloc(numOfWeightArrays * sizeof(double*));
+    for(int i = 0; i < numOfWeightArrays; i++) {
+        weightArrays[i] = (double*)malloc(weightSize * sizeof(double));
+        for(int j = 0; j < weightSize; j++)
+            weightArrays[i][j] = weightArraysToCopy[i][j];
+    }
+    
+    int biasSize = 3;
+    double bias[] = {1, 2, 3};
+
+    int numOfNodes = 3;
+
+    double* output = linearLayer(input, inputSize, weightArrays, weightSize, numOfWeightArrays, bias, biasSize, numOfNodes);
+
+    printf("Linear Linear Test:\n");
+    printf("\t Input size: %d\n", inputSize);
+    printf("\t Number of Weights: %d\n", numOfWeightArrays);
+    printf("\t Weight size: %d\n", weightSize);
+    printf("\t Bias size: %d\n", biasSize);
+    printf("\t Number of Nodes: %d\n", numOfNodes);
+    printf("\t Output size: %d\n", sizeof(output)/sizeof(output[0]));
+    printf("\t Output: \n\t\t");
+    for(int i = 0; i < numOfNodes; i++) {
+        printf(" %lf ", output[i]);
+    }
+    printf("\n\t Expected Output: [-1, 12, 8]");
+    printf("\n\t Expected ReLu: [0, 12, 8]");
+    printf("\n\t Expected Softmax: [0.000002, 0.982012, 0.017986]\n");
+
+    freeDoublePointers((void**)weightArrays, numOfWeightArrays);
+    free(output);
+}
+
+void testReLuLayer() {
+    int inputSize = 4;
+    double input[] = {1, 2, 3, 4}; 
+    
+    int numOfWeightArrays = 3;
+    int weightSize = 4;
+
+    double weightArraysToCopy[3][4] = {{1, 2, 3, -4}, {1, 1, 1, 1,}, {0.5, 0.5, 0.5, 0.5}};
+
+    double **weightArrays = (double**)malloc(numOfWeightArrays * sizeof(double*));
+    for(int i = 0; i < numOfWeightArrays; i++) {
+        weightArrays[i] = (double*)malloc(weightSize * sizeof(double));
+        for(int j = 0; j < weightSize; j++)
+            weightArrays[i][j] = weightArraysToCopy[i][j];
+    }
+    
+    int biasSize = 3;
+    double bias[] = {1, 2, 3};
+
+    int numOfNodes = 3;
+
+    double* output = linearLayer(input, inputSize, weightArrays, weightSize, numOfWeightArrays, bias, biasSize, numOfNodes);
+    output = reLuVector(output, numOfNodes);
+
+    printf("Linear Linear Test:\n");
+    printf("\t Input size: %d\n", inputSize);
+    printf("\t Number of Weights: %d\n", numOfWeightArrays);
+    printf("\t Weight size: %d\n", weightSize);
+    printf("\t Bias size: %d\n", biasSize);
+    printf("\t Number of Nodes: %d\n", numOfNodes);
+    printf("\t Output size: %d\n", sizeof(output)/sizeof(output[0]));
+    printf("\t Output: \n\t\t");
+    for(int i = 0; i < numOfNodes; i++) {
+        printf(" %lf ", output[i]);
+    }
+    printf("\n\t Expected Output: [-1, 12, 8]");
+    printf("\n\t Expected ReLu: [0, 12, 8]");
+    printf("\n\t Expected Softmax: [0.000002, 0.982012, 0.017986]\n");
+
+    freeDoublePointers((void**)weightArrays, numOfWeightArrays);
+    free(output);
+}
+
+void testSoftmaxLayer() {
+    int inputSize = 4;
+    double input[] = {1, 2, 3, 4}; 
+    
+    int numOfWeightArrays = 3;
+    int weightSize = 4;
+
+    double weightArraysToCopy[3][4] = {{1, 2, 3, -4}, {1, 1, 1, 1,}, {0.5, 0.5, 0.5, 0.5}};
+
+    double **weightArrays = (double**)malloc(numOfWeightArrays * sizeof(double*));
+    for(int i = 0; i < numOfWeightArrays; i++) {
+        weightArrays[i] = (double*)malloc(weightSize * sizeof(double));
+        for(int j = 0; j < weightSize; j++)
+            weightArrays[i][j] = weightArraysToCopy[i][j];
+    }
+    
+    int biasSize = 3;
+    double bias[] = {1, 2, 3};
+
+    int numOfNodes = 3;
+
+    double* output = linearLayer(input, inputSize, weightArrays, weightSize, numOfWeightArrays, bias, biasSize, numOfNodes);
+    output = softMaxVector(output, numOfNodes);
+
+    printf("Linear Linear Test:\n");
+    printf("\t Input size: %d\n", inputSize);
+    printf("\t Number of Weights: %d\n", numOfWeightArrays);
+    printf("\t Weight size: %d\n", weightSize);
+    printf("\t Bias size: %d\n", biasSize);
+    printf("\t Number of Nodes: %d\n", numOfNodes);
+    printf("\t Output size: %d\n", sizeof(output)/sizeof(output[0]));
+    printf("\t Output: \n\t\t");
+    for(int i = 0; i < numOfNodes; i++) {
+        printf(" %lf ", output[i]);
+    }
+    printf("\n\t Expected Output: [-1, 12, 8]");
+    printf("\n\t Expected ReLu: [0, 12, 8]");
+    printf("\n\t Expected Softmax: [0.000002, 0.982012, 0.017986]\n");
+
+    freeDoublePointers((void**)weightArrays, numOfWeightArrays);
+    free(output);
+}
+
+void testMNIST() {
+    // print pixels of first data in test dataset
+    int i;
+    for (i=0; i<784; i++) {
+        printf("%1.1f ", test_image[4][i]);
+        if ((i+1) % 28 == 0) putchar('\n');
+    }
+
+    // print first label in test dataset
+    printf("label: %d\n", test_label[4]);
+
+    char dummy;
+    // save image of first data in test dataset as .pgm file
+    printf("Continue: ");
+    scanf("%c", &dummy);
+
+    // show all pixels and labels in test dataset
+    print_mnist_pixel(test_image, NUM_TEST);
+    //print_mnist_label(test_label, NUM_TEST);
+}
+
+
+/************************************************************************************
+*                                                                                   *
+*   MAIN FUNCTION                                                                   *
+*                                                                                   *
+*************************************************************************************/
+
+int main() {
+    /* 
+        unsigned char train_image_char[NUM_TRAIN][SIZE];
+        unsigned char test_image_char[NUM_TEST][SIZE];
+        unsigned char train_label_char[NUM_TRAIN][1];
+        unsigned char test_label_char[NUM_TEST][1];
+    */
+    load_mnist();
+
+    /* Testing Conv2D [ WORKS ]*/
+    //testConv2D();
+
+    /* Testing Linear Node [ WORKS ]*/
+    //testLinearNode();
+
+    /* Testing Linear Layer [ WORKS ]*/
+    //testLinearLayer();
+
+    /* Testing ReLu Layer [ WORKS ]*/
+    //testReLuLayer();
+
+    /* Testing Softmax Layer [ WORKS ]*/
+    //testSoftmaxLayer();
+    
+    /* Testing MNIST Data [ WORKS ]*/
+    //testMNIST();
+
+    printf("\nEnd.");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/************************************************************************************
+*                                                                                   *
+*   DEPRECATED FUNCTIONS                                                            *
+*                                                                                   *
+*************************************************************************************/
 
 // Convolution2D Layer.
 // Performs element-wise dot-product between input and kernels with a stride of 1 and a bias
@@ -230,12 +516,6 @@ double** maxPool2D(double** input, int inputSize, int maxPoolSize, int stride) {
 }
 
 
-/************************************************************************************
-*                                                                                   *
-*   TEST FUNCTIONS                                                                  *
-*                                                                                   *
-*************************************************************************************/
-
 void testConv2D() {
     int numFilters = 1;
     int filterSize = 5;
@@ -285,117 +565,4 @@ void testConv2D() {
     freeTriplePointers((void***)output, numFilters, outputHeight);
     freeDoublePointers((void**)input, inputSize);
     freeTriplePointers((void***)filter, numFilters, filterSize);
-}
-
-void testLinearNode() {
-
-    double input[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    double weights[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    double bias = 5;
-
-    int inputSize = sizeof(input) / sizeof(input[0]);
-    int weightSize = sizeof(weights) / sizeof(weights[0]);
-
-    double output = linearNode(input, inputSize, weights, weightSize, bias);
-    printf("Linear Node Test:\n");
-    printf("\t Input size: %d\n", inputSize);
-    printf("\t Weight size: %d\n", weightSize);
-    printf("\t Output: %lf\n", output);
-    printf("\t Expected: 209.0");
-}
-
-void testLinearLayer() {
-    
-    int inputSize = 4;
-    double input[] = {1, 2, 3, 4}; 
-    
-    int numOfWeightArrays = 3;
-    int weightSize = 4;
-
-    double weightArraysToCopy[3][4] = {{1, 2, 3, -4}, {1, 1, 1, 1,}, {0.5, 0.5, 0.5, 0.5}};
-
-    double **weightArrays = (double**)malloc(numOfWeightArrays * sizeof(double*));
-    for(int i = 0; i < numOfWeightArrays; i++) {
-        weightArrays[i] = (double*)malloc(weightSize * sizeof(double));
-        for(int j = 0; j < weightSize; j++)
-            weightArrays[i][j] = weightArraysToCopy[i][j];
-    }
-    
-    int biasSize = 3;
-    double bias[] = {1, 2, 3};
-
-    int numOfNodes = 3;
-
-    double* output = linearLayer(input, inputSize, weightArrays, weightSize, numOfWeightArrays, bias, biasSize, numOfNodes, softMax);
-
-    printf("Linear Linear Test:\n");
-    printf("\t Input size: %d\n", inputSize);
-    printf("\t Number of Weights: %d\n", numOfWeightArrays);
-    printf("\t Weight size: %d\n", weightSize);
-    printf("\t Bias size: %d\n", biasSize);
-    printf("\t Number of Nodes: %d\n", numOfNodes);
-    printf("\t Output size: %d\n", sizeof(output)/sizeof(output[0]));
-    printf("\t Output: \n\t\t");
-    for(int i = 0; i < numOfNodes; i++) {
-        printf(" %lf ", output[i]);
-    }
-    printf("\n\t Expected ReLu: [0, 12, 8]");
-    printf("\n\t Expected Softmax: [0.000002, 0.982012, 0.017986]\n");
-
-    freeDoublePointers((void**)weightArrays, numOfWeightArrays);
-}
-
-void testMNIST() {
-    // print pixels of first data in test dataset
-    int i;
-    for (i=0; i<784; i++) {
-        printf("%1.1f ", test_image[4][i]);
-        if ((i+1) % 28 == 0) putchar('\n');
-    }
-
-    // print first label in test dataset
-    printf("label: %d\n", test_label[4]);
-
-    char dummy;
-    // save image of first data in test dataset as .pgm file
-    printf("Continue: ");
-    scanf("%c", &dummy);
-
-    // show all pixels and labels in test dataset
-    print_mnist_pixel(test_image, NUM_TEST);
-    //print_mnist_label(test_label, NUM_TEST);
-}
-
-/************************************************************************************
-*                                                                                   *
-*   MAIN FUNCTION                                                                   *
-*                                                                                   *
-*************************************************************************************/
-
-int main() {
-    /* 
-        unsigned char train_image_char[NUM_TRAIN][SIZE];
-        unsigned char test_image_char[NUM_TEST][SIZE];
-        unsigned char train_label_char[NUM_TRAIN][1];
-        unsigned char test_label_char[NUM_TEST][1];
-    */
-    load_mnist();
-
-    /* Testing Conv2D [ WORKS ]*/
-    //testConv2D();
-
-    /* Testing Linear Node [ WORKS ]*/
-    //testLinearNode();
-
-    /* Testing Linear Layer [ WORKS ]*/
-    //testLinearLayer();
-    
-    /* Testing MNIST Data [ WORKS ]*/
-    //testMNIST();
-
-    
-
-    printf("\nEnd.");
-
-    return 0;
 }
